1e866d9541192e457a40e6d5fcccb3128937f6dd
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-11-06 13:45:42 -0800

    Diferentes botones pre-configurados

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonScreen extends StatelessWidget {
   static const String name = 'home_screen';
@@ -10,7 +11,68 @@ class ButtonScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Buttons Screen'),
       ),
-      body: const Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back_ios_new_rounded),
+        onPressed: () {
+          context.pop();
+        },
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+        child: Wrap(
+          spacing: 12,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {}, child: const Text('Elevated Button')),
+            const ElevatedButton(
+                onPressed: null, child: Text('Elevated Button Disable')),
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.assessment_rounded),
+                label: const Text('Elevated Icon')),
+            OutlinedButton(
+                onPressed: () {}, child: const Text('Outlined Button')),
+            OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.outlined_flag),
+                label: const Text('Outlined Button Icon')),
+            TextButton(onPressed: () {}, child: const Text('Text Button')),
+            TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.text_format),
+                label: const Text('Text Button  Icon')),
+
+            // TODO: custom button
+
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.app_blocking_outlined)),
+
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.app_registration_rounded),
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(colors.primary),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
