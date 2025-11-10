11a16bad32fadd665b9d3fc88378402095ae3997
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-11-10 13:48:12 -0800

    Agregando nuevas rutas al archivo de barril

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,5 +1,11 @@
 // Esto es un archivo de barril
 
+export 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial.dart';
+export 'package:widgets_app/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_app/presentation/screens/snackbar_screen/snackbar_screen.dart';
+export 'package:widgets_app/presentation/screens/ui_controls/ui_controls.dart';
+export 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
 export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_app/presentation/screens/home/home_screen.dart';
