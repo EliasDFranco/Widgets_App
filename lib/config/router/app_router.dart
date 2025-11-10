0d99280bf05053fc6777a9d2684d35e6021e9b9d
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-11-10 13:47:50 -0800

    Agregando nuevas rutas para practicar GoRouter

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -17,4 +17,28 @@ final app_router = GoRouter(initialLocation: '/', routes: [
     name: CardsScreen.name,
     builder: (context, state) => const CardsScreen(),
   ),
+  GoRoute(
+      path: '/animated',
+      name: AnimatedScreen.name,
+      builder: (context, state) => const AnimatedScreen()),
+  GoRoute(
+      path: '/tutorial',
+      name: AppTutorialScreen.name,
+      builder: (context, state) => const AppTutorialScreen()),
+  GoRoute(
+      path: '/controls',
+      name: UiControlsScreen.name,
+      builder: (context, state) => const UiControlsScreen()),
+  GoRoute(
+      path: '/progress',
+      name: ProgressScreen.name,
+      builder: (context, state) => const ProgressScreen()),
+  GoRoute(
+      path: '/scroll',
+      name: InfiniteScrollScreen.name,
+      builder: (context, state) => const InfiniteScrollScreen()),
+  GoRoute(
+      path: '/snackbar',
+      name: SnackBarScreen.name,
+      builder: (context, state) => const SnackBarScreen())
 ]);
