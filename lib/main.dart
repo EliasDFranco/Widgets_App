43bb9aadc2bf71368cf39b69578e9b3a0d14dbbf
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-11-12 12:49:37 -0800

    Main corregido

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:widgets_app/config/router/app_router.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
-=======
-import 'package:widgets_app/presentation/screens/home_screen.dart';
-
-import 'config/theme/app_theme.dart';
->>>>>>> 01d4c482a0cfccba766b54dff42eb4a91d7207e3
 
 void main() => runApp(const MyApp());
 
@@ -18,14 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       routerConfig: app_router,
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
       theme: AppTheme(selectedColor: 1).getTheme(),
-=======
-      theme: AppTheme(selectedColor: 0).getTheme(),
-      title: 'Material App',
-      home: const HomeScreen(),
-      centerTitle: false,
->>>>>>> 01d4c482a0cfccba766b54dff42eb4a91d7207e3
     );
   }
 }
