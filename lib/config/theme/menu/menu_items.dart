0266f64b28091979637c34d733f419083a5eab32
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-11-12 14:14:01 -0800

    Agregando MenuItem de Progress Screen

--- lib/config/theme/menu/menu_items.dart
+++ lib/config/theme/menu/menu_items.dart
@@ -1,35 +1,33 @@
 import 'package:flutter/material.dart';
 
-
 class MenuItem {
   final String title;
   final String subTitle;
   final String link;
   final IconData icon;
 
-  const MenuItem({
-    required this.title,
-    required this.subTitle,
-    required this.link,
-    required this.icon
-  });
+  const MenuItem(
+      {required this.title,
+      required this.subTitle,
+      required this.link,
+      required this.icon});
 }
 
-
 const appMenuItems = <MenuItem>[
-
-
   MenuItem(
-    title: 'Botones', 
-    subTitle: 'Varios botones en Flutter', 
-    link: '/buttons', 
-    icon: Icons.smart_button_outlined
-  ),
-
+      title: 'Botones',
+      subTitle: 'Varios botones en Flutter',
+      link: '/buttons',
+      icon: Icons.smart_button_outlined),
   MenuItem(
-    title: 'Tarjetas', 
-    subTitle: 'Un contenedor estilizado', 
-    link: '/cards', 
-    icon: Icons.credit_card
+      title: 'Tarjetas',
+      subTitle: 'Un contenedor estilizado',
+      link: '/cards',
+      icon: Icons.credit_card),
+  MenuItem(
+    title: 'Progress Indicator',
+    subTitle: 'Generales y controladores',
+    link: '/progress',
+    icon: Icons.refresh_rounded,
   ),
 ];
