4fb6799309762fbf5d78f3c43f03680e5f319415
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-11-12 13:32:38 -0800

    Eliminando items que todavia no son necesarios

--- lib/config/theme/menu/menu_items.dart
+++ lib/config/theme/menu/menu_items.dart
@@ -20,13 +20,6 @@ const appMenuItems = <MenuItem>[
 
 
   MenuItem(
-    title: 'Riverpod Counter', 
-    subTitle: 'Introducción a riverpod', 
-    link: '/counter-river', 
-    icon: Icons.add
-  ),
-
-  MenuItem(
     title: 'Botones', 
     subTitle: 'Varios botones en Flutter', 
     link: '/buttons', 
@@ -39,56 +32,4 @@ const appMenuItems = <MenuItem>[
     link: '/cards', 
     icon: Icons.credit_card
   ),
-
-  MenuItem(
-    title: 'Progress Indicators', 
-    subTitle: 'Generales y controlados', 
-    link: '/progress', 
-    icon: Icons.refresh_rounded
-  ),
-
-  MenuItem(
-    title: 'Snackbars y diálogos', 
-    subTitle: 'Indicadores en pantalla', 
-    link: '/snackbars', 
-    icon: Icons.info_outline
-  ),
-
-  MenuItem(
-    title: 'Animated container', 
-    subTitle: 'Stateful widget animado', 
-    link: '/animated', 
-    icon: Icons.check_box_outline_blank_rounded
-  ),
-
-  MenuItem(
-    title: 'UI Controls + Tiles', 
-    subTitle: 'Una serie de controles de Flutter', 
-    link: '/ui-controls', 
-    icon: Icons.car_rental_outlined
-  ),
-
-  MenuItem(
-    title: 'Introducción a la aplicación', 
-    subTitle: 'Pequeño tutorial introductorio', 
-    link: '/tutorial', 
-    icon: Icons.accessible_rounded
-  ),
-
-  MenuItem(
-    title: 'InfiniteScroll y Pull', 
-    subTitle: 'Listas infinitas y pull to refresh', 
-    link: '/infinite', 
-    icon: Icons.list_alt_rounded
-  ),
-
-  MenuItem(
-    title: 'Cambiar tema', 
-    subTitle: 'Cambiar tema de la aplicación', 
-    link: '/theme-changer', 
-    icon: Icons.color_lens_outlined
-  ),
-
-
-
 ];
