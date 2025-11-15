c14c54bb70a02c6fc52bcf86e8521f16391357ba
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-11-15 11:06:28 -0800

    Agreando MenuItem de SnackBarScreen

--- lib/config/theme/menu/menu_items.dart
+++ lib/config/theme/menu/menu_items.dart
@@ -30,4 +30,9 @@ const appMenuItems = <MenuItem>[
     link: '/progress',
     icon: Icons.refresh_rounded,
   ),
+  MenuItem(
+      title: 'Snackbars y dialogos',
+      subTitle: 'Indicadores de pantalla',
+      link: '/snackbars',
+      icon: Icons.info_outline)
 ];
