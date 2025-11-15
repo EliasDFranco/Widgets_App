555699e48a1f671365c4c4502ee748b26c12e349
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-11-15 11:08:04 -0800

    Creando contenido para el SnakcBarScreen

--- lib/presentation/screens/snackbar_screen/snackbar_screen.dart
+++ lib/presentation/screens/snackbar_screen/snackbar_screen.dart
@@ -4,8 +4,25 @@ class SnackBarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
   const SnackBarScreen({super.key});
 
+  void showCustomSnackBar(BuildContext context) {
+    final snakbar = SnackBar(
+      content: const Text('El mundo es tuyp'),
+      action: SnackBarAction(label: 'Okk!', onPressed: () {}),
+      duration: const Duration(seconds: 3),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: snakbar));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('SnackBar'),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          label: const Text('Mostrar SnackBar'),
+          icon: const Icon(Icons.remove_red_eye_outlined),
+          onPressed: () => showCustomSnackBar(context),
+        ));
   }
 }
