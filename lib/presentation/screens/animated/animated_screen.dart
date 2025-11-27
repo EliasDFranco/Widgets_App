d1021225d913b00c017aa26cc133074639b0fd1d
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-11-27 13:47:42 -0800

    Cambiando animated_screen a Stateful Widget

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const name = 'animeted';
   const AnimatedScreen({super.key});
 
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
