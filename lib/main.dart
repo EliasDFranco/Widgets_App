42f69094314de32a2c4403664604633cc8dac5cd
Elias Franco Duarte <eliasdfranco@hotmail.com>
2026-01-10 06:06:24 -0800

    Introducción a Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/router/app_router.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(
+  ProviderScope(
+    child: const MyApp());
+  )
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Widgets',
       routerConfig: app_router,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 1).getTheme(),
+      theme: AppTheme(selectedColor: 1).getTheme(),n
     );
   }
 }
--- lib/presentation/screens/counter_riverpod/counter_riverpod.dart
+++ lib/presentation/screens/counter_riverpod/counter_riverpod.dart
@@ -1,15 +1,21 @@
-class CounterScreenRiverpod extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CounterScreenRiverpod extends ConsumerWidget {
   const CounterScreenRiverpod({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final int  clikcCounter = ref.watch( counterProvider);
+
     static const name = "counter_riverpod";
     return Scaffold(
       appBar: AppBar(
         title: Text("Riverpod Introduction",),
       ),
       body: Center(
-        child: Text("Valor de: ", style: Theme.of(context).textTheme.titleLarge),
+        child: Text("Valor de: $clickCounter ", style: Theme.of(context).textTheme.titleLarge),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
