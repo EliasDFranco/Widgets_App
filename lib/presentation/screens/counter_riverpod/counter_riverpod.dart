1ec1477fb2ee9aa61c5e4c8ad4b9f0ea835f024c
Elias Franco Duarte <eliasdfranco@hotmail.com>
2026-01-10 06:46:07 -0800

    Cambiar valores y modificadores

--- lib/presentation/screens/counter_riverpod/counter_riverpod.dart
+++ lib/presentation/screens/counter_riverpod/counter_riverpod.dart
@@ -8,18 +8,33 @@ class CounterScreenRiverpod extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
 
     final int  clikcCounter = ref.watch( counterProvider);
+    final bool isDarkMode = ref.watch( counterProvider);
 
     static const name = "counter_riverpod";
     return Scaffold(
       appBar: AppBar(
         title: Text("Riverpod Introduction",),
+        actions = {
+          IconButton(
+            // icon: Icon(Icons.light_mode_outlined),
+            icon: Icon(isDarkMode ? Icons.dark_mode_outlined : Icons.light_mode_outlined),
+            onPressed: ( ){
+            },
+              ref.read( isDarkThemeProvider.notifier).update((state) => !state);
+          ),
+        }
       ),
       body: Center(
         child: Text("Valor de: $clickCounter ", style: Theme.of(context).textTheme.titleLarge),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: (){},
+        onPressed: (){
+          ref.read( counterProvider.notifier ).state++;
+
+          / Other way
+        },
+          ref.read( counterProvider.notifier ).update((state) => state + 1);  
       ),
     );
   },
