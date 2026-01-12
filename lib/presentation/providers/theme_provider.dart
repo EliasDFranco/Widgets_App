2bb0783d9051631cbda636cd4eef9640edd303c8
Elias Franco Duarte <eliasdfranco@hotmail.com>
2026-01-12 12:55:07 -0800

    Provider selectedColorProvider | Explicaciones

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -6,3 +6,38 @@ final isDarkThemeProvider = StateProvider((ref) => false);
 
 // List of inmutable colors
 final colorListProvider = Provider((ref) => colorList);
+
+final selectedColorProvider = StateProvider((ref) => 0);
+
+
+
+/*
+
+Usa Provider (de Riverpod) para dependencias inmutables (repositorios, servicios) que no cambian,
+como un \(APIClient\) o un \(Logger\).
+Mientras que StateProvider es para estados simples y mutables (un número, un texto) que necesitan actualizarse en la UI,
+siendo ideal para contadores o el estado de un toggle, y para estados más complejos y lógicos,
+StateNotifierProvider con un StateNotifier es la opción recomendada. 
+
+Cuándo usar Provider (Riverpod)
+- Dependencias Inmutables: Cuando necesitas exponer un objeto que no cambia su estado interno, como un servicio de autenticación, una clase de acceso a datos o un configurador.
+- Inyección de Dependencias: Para inyectar servicios en tu aplicación de forma centralizada.
+- Estado de Solo Lectura: Si el dato que proporcionas solo se lee y no se modifica directamente desde el proveedor.
+
+Ejemplo de código: 
+  final apiClientProvider = Provider<ApiClient>((ref) {
+  return ApiClient();
+});
+
+
+Cuándo usar StateProvider (Riverpod)
+- Estados Simples: Para estados primitivos o sencillos que cambian con el tiempo y necesitan ser observados por la UI, como un bool, int, String.
+- Reconstrucciones Simples: Cuando la lógica de actualización del estado es mínima y no necesita una clase dedicada (como un StateNotifier).
+- Ejemplos: El valor de un Checkbox, el texto de un TextField, el estado de carga de un botón.  
+
+Ejemplo de código:
+final counterProvider = StateProvider<int>((ref) {
+  return 0;
+});
+
+*/
